05ca66a870b4221a8114b83016979664b3bec77f
Lsh0402 <ms777451@naver.com>
2024-04-19 02:28:29 +0900

    add mdule, add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = 'Unit_c',
+    version = '0.0.1',
+    description = 'Converts many different units',
+    author = 'leeseunghun',
+    url= 'https://github.com/Lsh0402/leepackage.git',
+    download_url = 'https://github.com/Lsh0402/leepackage.git',
+    packages= ['leepackage'],
+    classifiers =[
+        "Programming Language :: Python :: 3",
+    ]   
+)
