42199bf8e53270b099612c74440760c4a11a3669
Lsh0402 <162390227+Lsh0402@users.noreply.github.com>
2024-04-19 03:52:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setuptools.setup(
     version = '0.0.1',
     description = 'Converts many different units',
     author = 'leeseunghun',
-    url= 'https://github.com/Lsh0402/leepackage.git',
-    download_url = 'https://github.com/Lsh0402/leepackage.git',
-    packages= ['Unit_c'],
+    url= 'https://github.com/Lsh0402/leeUnit_c.git',
+    download_url = 'https://github.com/Lsh0402/leeUnit_c.git',
+    packages= ['leeUnit_c'],
     classifiers =[
         "Programming Language :: Python :: 3",
     ]   
